1c57bb7c4b5de7326d186fbf741fb497f09d2c1d
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2023-04-06 20:09:22 +0700

    update

---
+++ myfrontend/src/views/HomePage.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container is-widescreen">
+    <section class="hero">
+      <div class="hero-body">
+        <p class="title">My Stories</p>
+      </div>
+    </section>
+    <section class="section" id="app">
+      <div class="content">
+        <form method="GET" action="/">
+          <div class="columns">
+            <div class="column is-4 is-offset-2">
+              <input class="input" type="text" name="search" placeholder="ค้นชื่อบทความ" :value="search">
+            </div>
+            <div class="column is-2">
+              <input class="button" type="submit" value="Search">
+            </div>
+            <div class="column is-2">
+              <router-link to="/blog/create">
+                <input class="button" type="button" value="Create New Blog">
+              </router-link>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="content">
+        <div class="columns is-multiline">
+          <div class="column is-3" v-for="blog in blogs" :key="blog.id">
+            <div class="card">
+              <div class="card-image pt-5">
+                <figure class="image">
+                  <img style="height: 120px" :src="showimg(blog.file_path)" alt="Placeholder image" />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="title">{{ blog.title }}</div>
+                <div class="content">{{ blog.content }}</div>
+              </div>
+              <footer class="card-footer">
+                <router-link :to="{ name: 'BlogDetail', params: { id: blog.id } }" class="card-footer-item">Read
+                  more...</router-link>
+                <a href="/" class="card-footer-item" @click="addLike(blog.id)">
+                  <span class="icon-text">
+                    <span class="icon">
+                      <i class="far fa-heart"></i>
+                    </span>
+                    <span>Like {{ blog.like }}</span>
+                  </span>
+                </a>
+              </footer>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+  
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      blogs: null,
+      search: this.$route.query.search
+    };
+  },
+  methods: {
+    async addLike(id) {
+      try {
+        await axios.post(`http://localhost:3000/blogs/addlike/${id}`);
+      }
+      catch (error) {
+        console.log("error");
+      }
+    },
+    showimg(path) {
+      try {
+        let file = require(`../assets/static${path}`)
+        return file;
+      } catch (error) {
+        return "https://bulma.io/images/placeholders/640x360.png"
+      }
+    }
+  },
+  created() {
+    axios.get("http://localhost:3000/", { params: { search: this.search } })
+      .then((response) => {
+        this.blogs = response.data.blogs
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+</script>
+  
+<style scoped></style>
